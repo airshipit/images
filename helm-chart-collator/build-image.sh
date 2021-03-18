8a3f4265817d307a244938a5782753b378a44a2a
Anderson, Craig (ca846m) <ca846m@att.com>
2021-03-18 15:49:53 -0700

    Revert "Dex Chart Collator - Quay.io Docker image"
    This reverts commit a3c6b4454ae879ada9bc188b48429bfe53ae58b3.
    
    This broke proxy functionality and makes it impossible to build this
    project behind a proxy. Reverting until author completes proxy
    integration.
    
    Change-Id: I26b73a17736a28e9f45c62c100d85be25c648e44

--- helm-chart-collator/build-image.sh
+++ helm-chart-collator/build-image.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-set -xe
-
 if [[ $# != 1 ]]; then
-  printf "usage: $0 <filename>\n"
+  printf "usage: ./%s <filename>\n" "$0"
   exit 1
 fi
 
-IMAGE_NAME="${IMAGE_NAME:-helm-chart-collator}"
-DOCKER_REGISTRY="${DOCKER_REGISTRY:-quay.io}"
-IMAGE_PREFIX="${IMAGE_PREFIX:-airshipit}"
-IMAGE_TAG="${IMAGE_TAG:-latest}"
-
-image=${DOCKER_REGISTRY}/${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG}
-echo "Building the ${image}"
-docker build . -t $image --build-arg "CHARTS=\"$(cat "$1")\""
+docker build . -t helm-chart-collator --build-arg "CHARTS=\"$(cat "$1")\""
