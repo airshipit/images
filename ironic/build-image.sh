164611dcabe7d979da6524c5591d3007f2b20b32
Sreejith Punnapuzha <Sreejith.Punnapuzha@outlook.com>
2021-03-29 13:26:30 -0500

    Fix image publish job
    * Add nodeset so that image build and image publish job can run.
    * change pycrypto to cryptography in pip packages
    
    Signed-off-by: Sreejith Punnapuzha <Sreejith.Punnapuzha@outlook.com>
    Change-Id: Id04561790db6d3d0745bc5d17a525f7fc1c65092

--- ironic/build-image.sh
+++ ironic/build-image.sh
@@ -18,7 +18,7 @@ set -e
 : "${base_extra_build_args:="--force-rm --pull --no-cache"}"
 # Defaults for projects
 : "${ironic_profiles:="'fluent ipxe ipmi qemu tftp'"}"
-: "${ironic_pip_packages:="pycrypto python-openstackclient sushy"}"
+: "${ironic_pip_packages:="cryptography python-openstackclient sushy"}"
 : "${ironic_dist_packages:="ethtool lshw iproute2"}"
 # Image tag
 if [ -z "${IMAGE_TAG}" ]; then
